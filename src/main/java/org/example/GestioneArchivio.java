87a338bc2a4326ef2194f54b2e9d2364113b7496
Davide De Pasquale <davide.depasquale1998@gmail.com>
2025-01-19 17:46:09 +0100

    senza controlli.......... sono sfinito

--- src/main/java/org/example/GestioneArchivio.java
+++ src/main/java/org/example/GestioneArchivio.java
@@ -1,17 +1,25 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class GestioneArchivio {
     private List<ElemBib> archivio;
 
     public GestioneArchivio() {
         archivio = new ArrayList<>();
+        elementiInizialiArchivio();
     }
 
+    public void elementiInizialiArchivio(){
+        archivio.add(new Libri(143, "Pinocchio", 2022, 25, "Lebonwski", "Horror"));
+        archivio.add(new Libri(331, "La Bella Addormentata", 1995, 40, "Maccherone David", "Giallo"));
+        archivio.add(new Libri(111, "La Bella e la Bestia", 1980, 10, "Ciannarella Oscar", "Horror"));
+        archivio.add(new Libri(104, "Shrek",2000, 21, "Mastrangelo Daniele", "Commedy"));
+        archivio.add(new Riviste(788, "Gazzetta del Buongiornissimo", 1999, 3, Periodicita.SEMESTRALE));
+        archivio.add(new Riviste(611, "Paperino", 1959, 300, Periodicita.MENSILE));
+        archivio.add(new Riviste(413, "Gazzetta dello Sport", 1919, 10, Periodicita.SETTIMANALE));
+
+    }
     private boolean checkISBN(Long isbm) {
         boolean isValid = true;
         for (ElemBib a : archivio) {
@@ -166,6 +174,104 @@ public class GestioneArchivio {
 
         }
         System.out.println("Arrivederci.");
+    }
+    public void modificaElemTramiteISBN (Scanner sc){
+        System.out.println("Inserisci l'ISBN dell'elemento che vorresti modificare!");
+        long codISBN = Long.parseLong(sc.nextLine());
+       ElemBib elemento =  archivio.stream().filter(el -> el.getCodISBN() == codISBN).findFirst().orElse(null);
+       if(elemento != null){
+           System.out.println("Ecco l'elemento da te cercato : " + elemento);
+           System.out.println("Cosa vorresti modificare?");
+           System.out.println("1. Per modificare il titolo");
+           System.out.println("2. Per modificare l'anno di pubblicazione");
+           System.out.println("3. Per modificare il numero totale di pagine");
+           if(elemento instanceof Libri){
+               System.out.println("4. Per modificare l'autore");
+               System.out.println("5. Per modificare il genere");
+           } else if(elemento instanceof Riviste){
+               System.out.println("4. Per modificare il tipo di pubblicazione della rivista");
+           }
+           int sceltaRisposta = Integer.parseInt(sc.nextLine());
+           switch (sceltaRisposta){
+               case 1: {
+                   System.out.println("Inserisci il nuovo titolo : ");
+                   String newTitle = sc.nextLine();
+                   elemento.setTitolo(newTitle);
+                   System.out.println("Titolo modificato! Congratulazioni");
+                   break;
+               } case 2: {
+                   System.out.println("Inserisci il nuovo anno di pubblicazione");
+                   int newYear = Integer.parseInt(sc.nextLine());
+                   elemento.setAnnoPubblicazione(newYear);
+                   System.out.println("Anno di pubblicazione modificato");
+                   break;
+               } case 3: {
+                   System.out.println("Metti il nuovo numero di pagine");
+                   int newNumberofPage = Integer.parseInt(sc.nextLine());
+                   elemento.setNumeroPagine(newNumberofPage);
+                   System.out.println("Numero di pagine modificato!");
+                   break;
+               } case 4 : {
+                   if (elemento instanceof Libri){
+                       System.out.println("Inserisci il nuovo nome dell'autore!");
+                       String newAutore = sc.nextLine();
+                       ((Libri) elemento).setAutore(newAutore);
+                       System.out.println("Nome dell'Autore modificato!");
+
+                   } else if (elemento instanceof Riviste) {
+                       System.out.println("Modifica il tipo di pubblicazione della Rivista!");
+                       String newTipoDiPubblicazione = sc.nextLine().toUpperCase();
+                       Periodicita tipoDiPubblicazione = null;
+                       switch (newTipoDiPubblicazione){
+                           case "settimanale" : {
+                               tipoDiPubblicazione = Periodicita.SETTIMANALE;
+                               break;
+                           } case "mensile" : {
+                               tipoDiPubblicazione = Periodicita.MENSILE;
+                               break;
+                           } case "semestrale" : {
+                               tipoDiPubblicazione = Periodicita.SEMESTRALE;
+                               break;
+                           } default: {
+                               System.out.println("Tipo di pubblicazione inserito non valido! Le 3 opzioni disponibili sono : settimanale, mensile o semetrale");
+                               return;
+                           }
+                       }
+                       ((Riviste) elemento).setPeriodicita(tipoDiPubblicazione);
+                       System.out.println("Tipo di pubblicazione modificato!!");
+                   }
+                   break;
+               } case 5 : {
+                   if(elemento instanceof Libri){
+                       System.out.println("Inserisci il nuovo genere del Libro");
+                       String newGenere = sc.nextLine();
+                       ((Libri)elemento).setGenere(newGenere);
+                       System.out.println("Genere modificato!");
+                   }
+                   break;
+               } default: {
+                   System.out.println("Il cod ISBN fornito ( " + codISBN + " ) non appartiene a nessun elemento");
+               }
+           }
+       }
+
+
+    }
+    public void statsArchivio(Scanner sc){
+        long numLibri = archivio.stream().filter(elem -> elem instanceof Libri).count();
+        long numRiviste = archivio.stream().filter(elem ->elem instanceof Riviste).count();
+        Optional<ElemBib> elementoMaxPage = archivio.stream().max(Comparator.comparingInt(ElemBib::getNumeroPagine));
+        if (elementoMaxPage.isPresent()){
+            System.out.println("L'elemento col maggior numero di pagine è : " + elementoMaxPage.get());
+        } else {
+            System.out.println("Nessun elemento in archivio! Devi prima crearti gli elementi!!!");
+        }
+
+        double mediaDellePage = archivio.stream().mapToInt(ElemBib::getNumeroPagine).average().orElse(0);
+        System.out.println("Numero di Libri presenti in Archivio : " + numLibri);
+        System.out.println("Numero di Riviste presenti in Archivio : " + numRiviste);
+        System.out.println("Media pagine di tutti gli elementi in Archivio : " + mediaDellePage);
+
     }
 
 }
--- src/main/java/org/example/GestioneBib.java
+++ src/main/java/org/example/GestioneBib.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.time.Year;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,15 +10,11 @@ import java.util.Scanner;
 public class GestioneBib {
 
     public static void main(String[] args) {
-        //List<Riviste> listaRiviste = new ArrayList<>();
-        //List<Libri> listaDiLibri = new ArrayList<>();
-        //costruisciLista(listaDiLibri);
-        //costruisciListaRiviste(listaRiviste);
         GestioneArchivio gestioneBibl = new GestioneArchivio();
         Scanner sc = new Scanner(System.in);
 
 
-         boolean continua = true;
+        boolean continua = true;
         while (continua) {
             visualizzazioneMenuIniziale();
             String risposta = sc.nextLine();
@@ -39,11 +33,12 @@ public class GestioneBib {
                         case 1: {
                             System.out.println("Cosa vorresti fare? Pigia 1 per aggiungere un Libro o '2' per cercare un Libro");
                             int resp3 = Integer.parseInt(sc.nextLine());
-                            switch (resp3){
-                                case 1 : {
+                            switch (resp3) {
+                                case 1: {
                                     gestioneBibl.aggiungiLibro(sc, new Libri());
                                     break;
-                                } case 2 : {
+                                }
+                                case 2: {
                                     gestioneBibl.ricercaXISBN(sc);
                                     break;
                                 }
@@ -52,16 +47,17 @@ public class GestioneBib {
                         }
                         case 2: {
                             System.out.println("Cosa vorresti fare? Pigia '1' per aggiungere una nuova Rivista o '2' per cercare una Rivista");
-                           int resp4 = Integer.parseInt(sc.nextLine());
-                           switch (resp4){
-                               case 1 : {
-                                   gestioneBibl.aggiungiRivista(sc,new Riviste());
-                                   break;
+                            int resp4 = Integer.parseInt(sc.nextLine());
+                            switch (resp4) {
+                                case 1: {
+                                    gestioneBibl.aggiungiRivista(sc, new Riviste());
+                                    break;
 
-                               } case 2 :  {
-                                   gestioneBibl.ricercaXISBN(sc);
-                               }
-                           }
+                                }
+                                case 2: {
+                                    gestioneBibl.ricercaXISBN(sc);
+                                }
+                            }
                         }
                     }
                     break;
@@ -82,6 +78,14 @@ public class GestioneBib {
                     break;
                 }
                 case 5: {
+                    gestioneBibl.modificaElemTramiteISBN(sc);
+                    break;
+                }
+                case 6: {
+                    gestioneBibl.statsArchivio(sc);
+
+                }
+                case 7: {
                     System.out.println("Che tu possa passare una buonissima giornata :)");
                     continua = false;
                     sc.close();
@@ -91,32 +95,19 @@ public class GestioneBib {
                     System.out.println("Ma che cosa fai! Sei stupido? Devi mettere uno di quei numeri.");
                 }
             }
-
         }
         System.out.println("Roba in Archivio: ");
         System.out.println(gestioneBibl);
     }
+
     private static void visualizzazioneMenuIniziale() {
         System.out.println("Cosa vorresti fare?");
         System.out.println("Inserisci '1' per aggiungere un elemento libro o rivista");
         System.out.println("Inserisci '2' per rimuovere un elemento tramite codice ISBN");
         System.out.println("Inserisci '3' per visualizzare tutti gli articoli in archivio per un determinato anno di pubblicazione");
         System.out.println("Inserisci '4' per visualizzare tutti gli articoli in archivio per un determinato Autore");
-        System.out.println("Inserisci '5' per uscire");
-    }
-    private static void costruisciLista(List<Libri> listaDiLibri) {
-        listaDiLibri.add(new Libri(143, "Pinocchio", 2022, 25, "Lebonwski", "Horror"));
-        listaDiLibri.add(new Libri(331, "La Bella Addormentata", 1995, 40, "Maccherone David", "Giallo"));
-        listaDiLibri.add(new Libri(111, "La Bella e la Bestia", 1980, 10, "Ciannarella Oscar", "Horror"));
-        listaDiLibri.add(new Libri(104, "Shrek",2000, 21, "Mastrangelo Daniele", "Commedy"));
-        listaDiLibri.forEach(libri -> {
-            System.out.println(libri);
-        });
-    }
-    private static void costruisciListaRiviste(List<Riviste> listaRiviste) {
-        listaRiviste.add(new Riviste(788, "Gazzetta del Buongiornissimo", 1999, 3, Periodicita.SEMESTRALE));
-        listaRiviste.add(new Riviste(611, "Paperino", 1959, 300, Periodicita.MENSILE));
-        listaRiviste.add(new Riviste(413, "Gazzetta dello Sport", 1919, 10, Periodicita.SETTIMANALE));
-        listaRiviste.forEach(riviste -> System.out.println(riviste));
+        System.out.println("Inserisci '5' per modificare un elemento cercandolo tramite ISBN");
+        System.out.println("Inserisci '6' per visualizzare le Stats dell'Archivio!");
+        System.out.println("Inserisci '7' per uscire.");
     }
 }
